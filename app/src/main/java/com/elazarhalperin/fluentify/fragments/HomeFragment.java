7300643acc406b03dc0fe43eeb2835d21a471d54
Elazar Halperin <elazarhalper@gmail.com>
2023-02-08 19:53:12 +0200

    Added choose list and some icons

--- app/src/main/java/com/elazarhalperin/fluentify/fragments/HomeFragment.java
+++ app/src/main/java/com/elazarhalperin/fluentify/fragments/HomeFragment.java
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.elazarhalperin.fluentify.R;
@@ -24,7 +23,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learn, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
 
     }
 
